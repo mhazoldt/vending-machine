f446be8f1d3600ca125d7c6654a91dcfe253658e
Mark <mhazoldt+gh@gmail.com>
2017-08-21 15:14:37 -0700

    vending-machine

--- db/migration.sql
+++ db/migration.sql
@@ -1,26 +1,87 @@
--- create vending_machine database
+CREATE DATABASE  IF NOT EXISTS `vending_machine` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `vending_machine`;
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: localhost    Database: vending_machine
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.2.7-MariaDB
 
-CREATE SCHEMA `vending_machine` ;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `items`
+--
 
--- create items table
-CREATE TABLE `vending_machine`.`items` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  `price` INT NULL,
-  `stock` INT NULL,
+DROP TABLE IF EXISTS `items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `items` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `stock` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- transactions table
-CREATE TABLE `vending_machine`.`transactions` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `item` VARCHAR(45) NOT NULL,
-  `price` INT NOT NULL,
-  `money_provided` INT NOT NULL,
-  `change_returned` INT NOT NULL,
-  `stock` INT NOT NULL,
-  `ledger` INT NOT NULL,
+--
+-- Dumping data for table `items`
+--
+
+LOCK TABLES `items` WRITE;
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+INSERT INTO `items` VALUES (1,'Chips',100,4),(2,'BBQ Chips',100,10),(5,NULL,NULL,NULL),(6,NULL,NULL,NULL),(7,'soda',100,10),(8,NULL,NULL,NULL),(9,NULL,NULL,NULL),(10,NULL,NULL,NULL),(11,NULL,NULL,NULL),(12,NULL,NULL,NULL),(13,NULL,NULL,NULL),(14,NULL,NULL,NULL),(15,NULL,NULL,NULL),(16,NULL,NULL,NULL),(17,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transactions`
+--
+
+DROP TABLE IF EXISTS `transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transactions` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `item` varchar(45) NOT NULL,
+  `price` int(11) NOT NULL,
+  `money_provided` int(11) NOT NULL,
+  `change_returned` int(11) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `ledger` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `transactions`
+--
+
+LOCK TABLES `transactions` WRITE;
+/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+INSERT INTO `transactions` VALUES (1,'Chips',100,100,0,5,100),(2,'Chips',100,100,0,5,200),(3,'Chips',100,100,0,5,300),(4,'Chips',100,100,0,5,400),(5,'Chips',100,100,0,5,500),(6,'Chips',100,100,0,5,600),(7,'Chips',100,100,0,5,700),(8,'Chips',100,100,0,5,800),(9,'Chips',100,100,0,5,900),(10,'Chips',100,100,0,5,1000),(11,'Chips',100,100,0,5,1100),(12,'Chips',100,100,0,5,1200),(13,'Chips',100,100,0,5,1300),(14,'Chips',100,100,0,5,1400),(15,'Chips',100,100,0,5,1500),(16,'Chips',100,100,0,5,1600),(17,'Chips',100,100,0,5,1700),(18,'Chips',100,100,0,5,1800),(19,'Chips',100,150,50,4,1900),(20,'Chips',100,175,75,3,2000),(21,'Chips',100,175,75,2,2100),(22,'Chips',100,175,75,1,2200),(23,'Chips',100,175,75,0,2300),(24,'Chips',100,100,0,5,2400),(25,'Chips',100,175,75,4,2500);
+/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-08-21 15:13:46
